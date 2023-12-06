d69cd2d8060e90c2ae98c56a40396ecb57c9bae3
Léo Bournizien <73833842+Survivor2302@users.noreply.github.com>
2023-12-06 17:07:51 +0100

    fix

--- lib/view/welcomeview.dart
+++ lib/view/welcomeview.dart
@@ -66,6 +66,7 @@ class _UsernameInputPageState extends State<UsernameInputPage> {
             ElevatedButton(
               onPressed: () {
                 var exists = false;
+                robotViewModel.user?.robots.clear();
                 boxUser.values.forEach((element) {
                   if (element.name == _usernameController.text) {
                     element.robots.forEach((robot) {
